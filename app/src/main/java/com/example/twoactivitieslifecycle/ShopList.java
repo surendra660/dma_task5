9203819b2ab4a40bbdf0d2a5f43e1a6fc32282da
Suraj Sah <surajsah6041@gmail.com>
2022-04-05 14:04:20 +0545

    coding challenge completed

---
+++ app/src/main/java/com/example/twoactivitieslifecycle/ShopList.java
@@ -0,0 +1,20 @@
+package com.example.twoactivitieslifecycle;
+
+import java.util.HashMap;
+
+public class ShopList {
+
+    public HashMap<String,Integer> list=new HashMap<>();
+
+
+    public void addItem(String item){
+        if(list.containsKey(item)){
+            list.put(item, list.get(item)+1);
+        }else{
+            list.put(item,1);
+        }
+    }
+    public HashMap<String,Integer> getItems(){
+        return list;
+    }
+}
